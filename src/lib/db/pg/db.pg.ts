3621a90f229a79ce8119d045dbe712e7e7c2d9ed
officialpriyam <officialpriyammishra2006@gmail.com>
2026-01-24 20:07:34 +0530

    Update db.pg.ts

--- src/lib/db/pg/db.pg.ts
+++ src/lib/db/pg/db.pg.ts
@@ -1,11 +1,11 @@
-// import { Logger } from "drizzle-orm";
 import { drizzle as drizzlePg } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
-// class MyLogger implements Logger {
-//   logQuery(query: string, params: unknown[]): void {
-//     console.log({ query, params });
-//   }
-// }
-export const pgDb = drizzlePg(process.env.POSTGRES_URL!, {
-  //   logger: new MyLogger(),
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL!,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+export const pgDb = drizzlePg(pool);
